71b4b616db1560138f9b7fe3cfa705d6bc0008c4
HclassHelper <email@example.com>
2023-01-04 12:01:09 +0800

    v1.3主界面更换

---
+++ js/newpage.js
@@ -0,0 +1,20 @@
+// 跳转函数
+function openpage(num){
+  let hrefList = [
+    "https://thisis.host/",
+    "./randomnumberbuilder.html",
+    "./timers/countdown.html",
+    "./timers/beijingtime.html",
+    "#",
+    "./showscreen.html",
+    "./toupiao.html",
+    "./oncalendar/index.html",
+    "./onwoodfish/index.html",
+    "./about.html",
+  ]
+  window.location.href = hrefList[num];
+}
+
+// function open(){
+//   window.location.href = "https://thisis.host";
+// }
