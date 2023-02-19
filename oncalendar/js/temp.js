3e2ce61e533d938399a4af21c34bfc6eb728bbe0
TengKongTec <2219655314@qq.com>
2023-02-19 11:51:32 +0800

    23/02/19 v1.4.1

--- oncalendar/js/temp.js
+++ oncalendar/js/temp.js
@@ -2,12 +2,18 @@
 
 //添加临时值日生
 function addCleanStudent(){
-    let name = prompt("添加今日值日小组,注意：只能添加一次,错了要刷新重来");
+    let name = prompt("添加今日值日小组,\n注意：只能添加一次,错了要刷新重来");
     document.getElementById("cleanStudentMain").innerHTML = name;
 }
 
 //添加临时擦黑板
 function addCleanBlackboardStudent(){
-    let name = prompt("添加今日临时擦黑板同学，注意：只能添加一次，要不然要刷新重来");
+    let name = prompt("添加今日临时擦黑板同学，\n注意：只能添加一次，要不然要刷新重来");
     document.getElementById("cleanBlackboardStudentMain").innerHTML = name;
+}
+
+//添加临时晚课
+function addNightClass(){
+    let name = prompt("添加今日晚课，\n注意：只能添加一次，要不然要刷新重来");
+    document.getElementById("toEatMain").innerHTML = name;
 }
